76dfc5f4011bc597faf1e6355b2ff33e2b86fdf8
Arkadiusz <asmilewski@gmail.com>
2018-02-06 08:18:07 +0100

    commit to check url origin

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DashboardPage = () => (
   <div className="dashBoardBox">
     <div className='list'>Column with list of topics and my projects</div>
-    <div>description of a topic of the list (toogled)</div>
+    <div>description of a topic of the list </div>
   
   </div>
 )
