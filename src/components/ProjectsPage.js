74af03ac1511f9b65bc0cd2f4a21d34c2ca4b93a
Arkadiusz <asmilewski@gmail.com>
2018-02-12 09:29:37 +0100

    Four applications included in the project section

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -6,9 +6,52 @@ import LinksComponent from './LinksComponent';
 const ProjectsPage = (props) => (
   <div className="component-box">
     <LinksComponent />
-    <Link to="projects-page/1">Item One     </Link>
-    <Link to="projects-page/2">Item Two     </Link>
-    <Link to="projects-page/3">Item three </Link>
+    <Link to="projects-page/1">
+      <div>
+        <h3>ThinkTwiceApp</h3>
+        <div>
+          <img src="" alt="ThinkTwiceImg"/>
+        </div>
+        <p>
+          description of Think Twice app
+        </p>
+      </div>
+    </Link>
+
+    <Link to="projects-page/2">
+      <div>
+        <h3>IndecisionApp</h3>
+        <div>
+          <img src="" alt="IndecisionAppImage"/>
+        </div>
+        <p>
+          description of IndecisionApp
+        </p>
+      </div>
+    </Link>
+
+    <Link to="projects-page/3">
+      <div>
+        <h3>ThinkTwiceApp</h3>
+        <div>
+          <img src="" alt="WeatherApp"/>
+        </div>
+        <p>
+          description of the WeatherApp
+        </p>
+      </div>
+    </Link>
+    <Link to="projects-page/4">
+      <div>
+        <h3>Cat Poster Store</h3>
+        <div>
+          <img src="" alt="Cat Poster Store"/>
+        </div>
+        <p>
+          description of the Cat Poster Store
+        </p>
+      </div>
+  </Link>
     <p>This page is for the item with the id of {props.match.params.id}</p>
   </div>
 );
