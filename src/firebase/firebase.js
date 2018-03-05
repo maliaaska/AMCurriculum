bc015ee320756128db9998539863a1b1c4505372
Arkadiusz <asmilewski@gmail.com>
2018-03-05 13:33:57 +0100

    changing database URL

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,6 +11,6 @@ const config = {
 
 firebase.initializeApp(config);
 
-const database = firebase.database();
+const database = firebase.databaseResume();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 export { firebase, googleAuthProvider, database as default }
