b52b2c27c54e2fa80a47d10354d5d8d2b4dfc4df
Arkadiusz <asmilewski@gmail.com>
2018-03-06 08:34:29 +0100

    package.json and firebase.js changes

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -11,6 +11,6 @@ const config = {
 
 firebase.initializeApp(config);
 
-const database = firebase.databaseResume();
+const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 export { firebase, googleAuthProvider, database as default }
