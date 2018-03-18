0d10ef22e56af44ebedbbe1be8a47323b1c92a5c
Arkadiusz <asmilewski@gmail.com>
2018-03-18 11:21:44 +0100

    home page responsive design completed

--- src/components/LinksComponent.js
+++ src/components/LinksComponent.js
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom';
 
 const LinksComponent = (props) => (
   <div className='list'>
-    <div><NavLink to="/homepage" activeClassName="is-active" >Home</NavLink></div>
-    <div><NavLink to="/homepage/projects-page" activeClassName="is-active">Projects</NavLink></div>
-    <div><NavLink to="/homepage/profile-page" activeClassName="is-active" >Profile</NavLink></div>
+    <div><NavLink to="/homepage" activeClassName="is-active"><p>Home</p></NavLink></div>
+    <div><NavLink to="/homepage/projects-page" activeClassName="is-active"><p>Projects</p></NavLink></div>
+    <div><NavLink to="/homepage/profile-page" activeClassName="is-active"><p>Profile</p></NavLink></div>
   </div>
 );
 
