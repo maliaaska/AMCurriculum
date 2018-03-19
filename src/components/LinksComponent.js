41a99d83f27f61e9fb2332156516c2cc6fc6fb84
Arkadiusz <asmilewski@gmail.com>
2018-03-19 10:38:35 +0100

    NavLink none underscore setup

--- src/components/LinksComponent.js
+++ src/components/LinksComponent.js
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom';
 
 const LinksComponent = (props) => (
   <div className='list'>
-    <div><NavLink to="/homepage" activeClassName="is-active"><p>Home</p></NavLink></div>
-    <div><NavLink to="/homepage/projects-page" activeClassName="is-active"><p>Projects</p></NavLink></div>
-    <div><NavLink to="/homepage/profile-page" activeClassName="is-active"><p>Profile</p></NavLink></div>
+    <div><NavLink className='nav-link' to="/homepage" activeClassName="is-active"><p>Home</p></NavLink></div>
+    <div><NavLink className='nav-link' to="/homepage/projects-page" activeClassName="is-active"><p>Projects</p></NavLink></div>
+    <div><NavLink className='nav-link' to="/homepage/profile-page" activeClassName="is-active"><p>Profile</p></NavLink></div>
   </div>
 );
 
