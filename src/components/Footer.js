980d2545df10111c326ca30cf3170eef71a9ac52
Arkadiusz <asmilewski@gmail.com>
2018-04-20 13:00:08 +0200

    logos of technologies in the footer are interactive

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,42 +2,42 @@ import React from 'react';
 
 const Footer = () => (
   <div className='footer'>
-    <a rel="pulse-grow" className="button pulse-grow">
+    <a rel="grow" className="button grow">
       <div className='tech-picture'>
         <img src="/images/html-css.png" alt="html-css"/>
       </div>
     </a>
-    <a rel="pulse-grow" className="button pulse-grow">
+    <a rel="grow" className="button grow">
       <div className='tech-picture'>
         <img src="/images/javascript.png" alt="javascript"/>
       </div>
     </a>
-    <a rel="pulse-grow" className="button pulse-grow">
+    <a rel="grow" className="button grow">
       <div className='tech-picture'>
         <img src="/images/react.png" alt="react.js"/>
       </div>
     </a>
-    <a rel="pulse-grow" className="button pulse-grow">
+    <a rel="grow" className="button grow">
       <div className='tech-picture'>
         <img src="/images/redux.png" alt="redux"/>
       </div>
     </a>
-    <a rel="pulse-grow" className="button pulse-grow">
+    <a rel="grow" className="button grow">
       <div className='tech-picture'>
         <img src="/images/firebase.png" alt="firebase"/>
       </div>
     </a>
-    <a rel="pulse-grow" className="button pulse-grow">
+    <a rel="grow" className="button grow">
       <div className='tech-picture'>
         <img src="/images/sass.png" alt="sass"/>
       </div>
     </a>
-    <a rel="pulse-grow" className="button pulse-grow">
+    <a rel="grow" className="button grow">
       <div className='tech-picture'>
         <img src="/images/heroku.png" alt="heroku"/>
       </div>
     </a>
-    <a rel="pulse-grow" className="button pulse-grow">
+    <a rel="grow" className="button grow">
       <div className='tech-picture'>
         <img src="/images/node.png" alt="node"/>
       </div> 
