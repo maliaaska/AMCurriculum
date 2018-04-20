32c8c4ffaf06f5b88f10263f8cbc1de7daf61efc
Arkadiusz <asmilewski@gmail.com>
2018-04-20 12:47:09 +0200

    project box has a border-bottom included, anchors included in the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,30 +2,46 @@ import React from 'react';
 
 const Footer = () => (
   <div className='footer'>
-    <div className='tech-picture'>
-      <img src="/images/html-css.png" alt="html-css"/>
-    </div>
-    <div className='tech-picture'>
-      <img src="/images/javascript.png" alt="javascript"/>
-    </div>
-    <div className='tech-picture'>
-      <img src="/images/react.png" alt="react.js"/>
-    </div>
-    <div className='tech-picture'>
-      <img src="/images/redux.png" alt="redux"/>
-    </div>
-    <div className='tech-picture'>
-      <img src="/images/firebase.png" alt="firebase"/>
-    </div>
-    <div className='tech-picture'>
-      <img src="/images/sass.png" alt="sass"/>
-    </div>
-    <div className='tech-picture'>
-      <img src="/images/heroku.png" alt="heroku"/>
-    </div>
-    <div className='tech-picture'>
-      <img src="/images/node.png" alt="node"/>
-    </div>
+    <a rel="pulse-grow" className="button pulse-grow">
+      <div className='tech-picture'>
+        <img src="/images/html-css.png" alt="html-css"/>
+      </div>
+    </a>
+    <a rel="pulse-grow" className="button pulse-grow">
+      <div className='tech-picture'>
+        <img src="/images/javascript.png" alt="javascript"/>
+      </div>
+    </a>
+    <a rel="pulse-grow" className="button pulse-grow">
+      <div className='tech-picture'>
+        <img src="/images/react.png" alt="react.js"/>
+      </div>
+    </a>
+    <a rel="pulse-grow" className="button pulse-grow">
+      <div className='tech-picture'>
+        <img src="/images/redux.png" alt="redux"/>
+      </div>
+    </a>
+    <a rel="pulse-grow" className="button pulse-grow">
+      <div className='tech-picture'>
+        <img src="/images/firebase.png" alt="firebase"/>
+      </div>
+    </a>
+    <a rel="pulse-grow" className="button pulse-grow">
+      <div className='tech-picture'>
+        <img src="/images/sass.png" alt="sass"/>
+      </div>
+    </a>
+    <a rel="pulse-grow" className="button pulse-grow">
+      <div className='tech-picture'>
+        <img src="/images/heroku.png" alt="heroku"/>
+      </div>
+    </a>
+    <a rel="pulse-grow" className="button pulse-grow">
+      <div className='tech-picture'>
+        <img src="/images/node.png" alt="node"/>
+      </div> 
+    </a>
   </div>
 );
 
