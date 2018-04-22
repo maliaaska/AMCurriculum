325acdcb8d993c4d5a1307556871de2ab8d0b3f9
Arkadiusz <asmilewski@gmail.com>
2018-04-22 09:59:36 +0200

    restructering for big screen purposes

--- src/components/LinksComponent.js
+++ src/components/LinksComponent.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const LinksComponent = (props) => (
-  <div className='list'>
+  <div className='tab-box'>
+  <div className="content-container">
     <div><NavLink className='nav-link' to="/homepage" activeClassName="is-active"><p>Home</p></NavLink></div>
     <div><NavLink className='nav-link' to="/homepage/projects-page" activeClassName="is-active"><p>Projects</p></NavLink></div>
     <div><NavLink className='nav-link' to="/homepage/profile-page" activeClassName="is-active"><p>Profile</p></NavLink></div>
+  </div>
+    
   </div>
 );
 
