f62b3123417708935afc83b0c43cd677521203b2
Arkadiusz <asmilewski@gmail.com>
2018-04-22 10:54:25 +0200

    Tabs' div's height set up correctly

--- src/components/TabsComponent.js
+++ src/components/TabsComponent.js
@@ -2,14 +2,11 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const TabsComponent = (props) => (
-  <div className='tab-box'>
-  <div className="content-container">
-    <div><NavLink className='nav-link' to="/homepage" activeClassName="is-active"><p>Home</p></NavLink></div>
-    <div><NavLink className='nav-link' to="/homepage/projects-page" activeClassName="is-active"><p>Projects</p></NavLink></div>
-    <div><NavLink className='nav-link' to="/homepage/profile-page" activeClassName="is-active"><p>Profile</p></NavLink></div>
-  </div>
-    
-  </div>
+    <div className='tab-box'>
+      <div><NavLink className='nav-link' to="/homepage" activeClassName="is-active"><p>Home</p></NavLink></div>
+      <div><NavLink className='nav-link' to="/homepage/projects-page" activeClassName="is-active"><p>Projects</p></NavLink></div>
+      <div><NavLink className='nav-link' to="/homepage/profile-page" activeClassName="is-active"><p>Profile</p></NavLink></div>
+    </div>  
 );
 
 export default TabsComponent;
