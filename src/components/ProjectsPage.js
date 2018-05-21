c23c851df0afa858688a32aa09bfb932df3f7a9b
Arkadiusz <asmilewski@gmail.com>
2018-05-21 09:51:59 +0200

    home-page box 2 adjusted for mobile size

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -15,7 +15,7 @@ const ProjectsPage = (props) => (
       <a href="https://react-thinktwiceapp.herokuapp.com/" target='_blank' rel="wobble-horizontal" className="wobble-horizontal">
         <div className='project-box'>
           <div className='app-description'>
-            <h5>ThinkTwiceApp</h5>
+            <h3>ThinkTwiceApp</h3>
             <p>
               ThinkTwiceApp has been built with React. It is an app what helps you track you all you expenses you have made. 
               App allows you to Log in with you Gmail what creates your individual, unique account.
@@ -29,7 +29,7 @@ const ProjectsPage = (props) => (
       <a href="https://indecision-app-mil.herokuapp.com/" target='_blank' rel="wobble-horizontal" className="wobble-horizontal">
         <div className='project-box'>
           <div className='app-description'>
-            <h5>IndecisionApp</h5>           
+            <h3>IndecisionApp</h3>           
             <p>
               IndecisionApp has been built with React.js.
               It is a simple app that helps you to make a decision in case you are not sure what to do. 
@@ -43,7 +43,7 @@ const ProjectsPage = (props) => (
       <a href="https://cat-poster-store.herokuapp.com/" target="_blank" rel="wobble-horizontal" className="wobble-horizontal">
         <div className='project-box'>
           <div className='app-description'>
-            <h5>Cat Poster Store</h5>           
+            <h3>Cat Poster Store</h3>           
             <p>
               The "Cat Poster Store" is a e-store where you can choose your favorite postcard with a Cat. When you click a button popup a random text in the viewChild!
             </p>
