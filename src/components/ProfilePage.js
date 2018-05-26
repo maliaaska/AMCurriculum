bce459595b9b911c617ae7f5fb600b26bdfe59b4
Arkadiusz <asmilewski@gmail.com>
2018-05-26 09:53:34 +0200

    ProjectPage link-image adjusted, contact-box adjusted

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -8,7 +8,7 @@ const ProfilePage = () => (
       <div className='description-box'>
         <h3>My short story</h3>
         <p className='page-description'>
-          Arkadiusz Szymon Milewski and was born 12 of May 1990 Warsaw, Poland.
+          Arkadiusz Szymon Milewski was born 12 of May 1990 Warsaw, Poland.
           Currently lives in Amsterdam, The Netherlands.
           Born Traveller and Sportsman, since second half of 2016 pssionated about programming.
           Did a course of Web Development Jun-August 2017 in Barcelona.
@@ -19,23 +19,35 @@ const ProfilePage = () => (
     </div>
     <div className='component-box-2'>
       <div className='box-contact'>
-        <h4>Contact details</h4>
+        <h2>Contact details</h2>
         <div className="link-image">
           <a href="mailto:asmilewski@gmail.com" target='_blank'>
             <img src="/images/gmail.png" alt="Gmail_logo"/>
-            <span>asmilewski@gmail.com</span>
+            <div>
+              <p>
+                asmilewski@gmail.com
+              </p>
+            </div>
           </a>
         </div>
         <div className="link-image">
           <a href="https://www.linkedin.com/in/arkadiusz-milewski-48977155/" target='_blank'>
             <img src="/images/linkedin.png" alt="linkedIn_logo"/>
-            <span>My LinkedIn profile</span>
+            <div>
+            <p>
+              My LinkedIn profile
+            </p>
+            </div>
           </a>
         </div>
         <div className="link-image">
           <a href="https://github.com/maliaaska" target='_blank'>
             <img src="/images/github.png" alt="GitHub_logo"/>
-            <span>My GitHub profile</span>
+            <div>
+            <p>
+              My GitHub profile
+            </p> 
+            </div>
           </a>
         </div>
       </div>
