4b7f9e136667dcb29225007182168ee46e642a51
Arkadiusz <asmilewski@gmail.com>
2018-07-17 10:43:55 +0200

    NavLink goes to singular project page without opening a new tab

--- src/components/ProjectsPage.js
+++ src/components/ProjectsPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import TabsComponent from './TabsComponent';
 import ThinkTwiceDescr from './ThinkTwiceDescr';
 import CatPosterStoreDescr from './CatPosterStoreDescr';
 import IndecisionAppDescr from './IndecisionAppDescr';
+
 const ProjectsPage = () => (
   <div className="component-box">
     <div className='component-box-1'>
@@ -13,15 +14,15 @@ const ProjectsPage = () => (
       </div>
     </div>
     <div className='component-box-2-project first-project-box'>
-      <Link to='/homepage/think-twice' target="_blank" rel="wobble-horizontal" className="wobble-horizontal">
+      <NavLink to='/homepage/think-twice' rel="wobble-horizontal" className="wobble-horizontal">
         <ThinkTwiceDescr />
-      </Link>
-      <Link to="/homepage/indecision-app" target="_blank" rel="wobble-horizontal" className="wobble-horizontal">
+      </NavLink>
+      <NavLink to="/homepage/indecision-app" rel="wobble-horizontal" className="wobble-horizontal">
         <IndecisionAppDescr />
-      </Link>
-      <Link to="/homepage/cat-poster-store" target="_blank" rel="wobble-horizontal" className="wobble-horizontal">
+      </NavLink>
+      <NavLink to="/homepage/cat-poster-store" rel="wobble-horizontal" className="wobble-horizontal">
         <CatPosterStoreDescr />
-      </Link>
+      </NavLink>
     </div>
   </div>
 );
