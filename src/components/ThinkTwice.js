849ce07a86897958166788a6d3ffca77582b480f
Arkadiusz <asmilewski@gmail.com>
2018-08-21 10:33:37 +0200

    picture on small devices displayed in the center when inside of project component

--- src/components/ThinkTwice.js
+++ src/components/ThinkTwice.js
@@ -8,20 +8,24 @@ const ThinkTwice = () => (
       <TabsComponent />
     </div>
     <div className='component-box-2-project first-project-box'>
-      <div>
-        <h2>
-          This is from the Think twice component!
-        </h2>
-      </div>
-      <div className='app-picture'>
-        <img src="/images/money.png" alt="ThinkTwiceImg" id='think-twice-icon'/>
-      </div>
-      <div className='list'>
-        <ul>
-        </ul>
-        <input type="text" className='addItemInput'></input>
-        <button className='addItemButton' onClick= { addComment } >Add Comment</button>
-        <div id='content'></div>
+      <div className='app-container'>
+        <div>
+          <p>
+            This is from the Think twice component!
+          </p>
+        </div>
+        <a href="https://react-thinktwiceapp.herokuapp.com/" target='_blank' rel="wobble-horizontal" className="app-anchor">
+          <div className='app-picture'>
+            <img src="/images/money.png" alt="ThinkTwiceImg" id='think-twice-icon'/>
+          </div>
+        </a>
+        <div className='list'>
+          <ul>
+          </ul>
+            <input type="text" className='addItemInput'></input>
+            <button className='addItemButton' onClick= { addComment } >Add Comment</button>
+          <div id='content'></div>
+        </div>
       </div>
     </div>
   </div>
@@ -29,5 +33,4 @@ const ThinkTwice = () => (
 
 export default ThinkTwice;
 
-// <a href="https://react-thinktwiceapp.herokuapp.com/" target='_blank' rel="wobble-horizontal" className="wobble-horizontal">
-// </a>
+
